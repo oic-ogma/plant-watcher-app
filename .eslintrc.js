b8fb6d2db8a0dfb115274eb9d13e975443e6a567
davis.kemlers@iohk.io <davis.kemlers@iohk.io>
2017-08-12 22:51:37 +0900

    6 - eslintのファイルを追加
    問題
    共通するリンターがありません。
    
    解決策
    eslintのNPMをインストールして、eslintの設定ファイルを過去のプロジェックト
    を元にして作成。

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': ['eslint:recommended', 'plugin:react/recommended'],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'experimentalObjectRestSpread': true,
+            'jsx': true,
+            'ecmaVersion': 6
+        },
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'space-infix-ops': ['error', {'int32Hint': false}],
+        'object-curly-spacing': [2, 'always'],
+        'space-in-parens': ['error', 'never'],
+        'camelcase': 'error',
+        'no-var': 2,
+        'prefer-arrow-callback': 2,
+        'func-style': [2, 'expression'], 
+        'no-trailing-spaces': 2,
+        'keyword-spacing': 2, 
+        'space-before-blocks': 2, 
+
+    
+        /** React **/
+        'react/display-name': 0,
+        'react/jsx-curly-spacing': [2, 'always'],
+        'react/jsx-equals-spacing': [2, 'never'],
+        'react/jsx-indent': [2, 'tab'| 2],
+        'react/jsx-wrap-multilines': 2,
+        'react/prop-types': 0,
+    }
+};
