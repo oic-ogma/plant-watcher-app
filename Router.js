af4b50efebbbd177d4aae7782bcaa0e0f05663b8
youmjww <youmjww@gmail.com>
2017-08-16 17:09:41 +0900

    Revert "11 - ホーム画面の追加"

--- Router.js
+++ Router.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import TestComponent from './components/TestComponent';
-import Home from './components/Home';
 
-const RouterComponent = () => (
-  <Router getSceneStyle={()=>({ backgroundColor: '#fff' })}>
-    <Scene key="root" hideNavBar={true}>
-      <Scene key="auth">
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Scene key="root">
         <Scene key="temporary" component={TestComponent} title="一時的なページ" />
       </Scene>
-
-      <Scene key="main">
-        <Scene key="home" component={Home} title="ホーム" />
-      </Scene>
-    </Scene>
-  </Router>
-);
+    </Router>
+  );
+};
 
 export default RouterComponent;
