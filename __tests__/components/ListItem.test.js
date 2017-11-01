927e24a1184bab5448b60fa9240fd1b0bf9b11ae
youmjww <b4234@oic.jp>
2017-11-01 21:37:56 +0900

    14 - ListItemのスナップショットテストを追加
    問題
    ListItemのテストがなかった
    
    解決策
    テストを書いた
    
    その他
    このコンポーネントは、カードに表示するだけのコンポーネントなので、actionとreducerのテストは無くても良いものとします。

---
+++ __tests__/components/ListItem.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ListItem } from '../../src/components/common/ListItem';
+import renderer from 'react-test-renderer';
+
+describe('記事の表示', () => {
+  it('snapshot-普通', () => {
+    const plantName = 'testName';
+    const articleContents = 'this is test contents. \n this contens is test data.';
+    const error = '';
+    const tree = renderer.create(<ListItem plantName={plantName} articleContents={articleContents} error={error} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('snapshot-エラー', () => {
+    const plantName = '';
+    const articleContents = '';
+    const error = 'error message';
+
+    const tree = renderer.create(<ListItem plantName={plantName} articleContents={articleContents} error={error} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
