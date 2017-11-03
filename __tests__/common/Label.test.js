eecda2d1bcb990a0566a0aa3c625864b5e0c2a3a
70mas <b4024@oic.jp>
2017-11-03 17:16:22 +0900

    32 - テストの更新、追加
    問題
    AddArticleのsnapshotが失敗してる。
    LabelとFormInputのテストがない。
    
    解決策
    AddArticleのテストを更新
    LabelとFormInputのテストを追加

---
+++ __tests__/common/Label.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Label } from '../../src/components/common';
+import renderer from 'react-test-renderer';
+
+describe('LabelComponent', () => {
+  it('snapshot-普通', () => {
+    const tree = renderer.create(<Label/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('snapshot-placeholderが渡されている', () => {
+    const tree = renderer.create(<Label placeholder='ひまわり' />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
