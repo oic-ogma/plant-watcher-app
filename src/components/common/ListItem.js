3554930fb07ee8258212692dfaf7a2889d91eddf
70mas <b4024@oic.jp>
2017-11-07 17:06:54 +0900

    14  - いらないファンクションを削除
    問題
    いらないファンクションが残っていた
    
    解決策
    いらないファンクションを削除

--- src/components/common/ListItem.js
+++ src/components/common/ListItem.js
@@ -2,14 +2,6 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { CardSection, Card, Error } from '../common';
 
-const sliceArticle = articleContent => {
-  const shortArticle = articleContent.slice(0,73);
-  if (shortArticle.length > 72) {
-    return `${shortArticle} ...`;
-  }
-  return articleContent;
-};
-
 const ListItem = ({ plantName, articleContents, error }) => {
   return (
     <Card>
@@ -25,7 +17,7 @@ const ListItem = ({ plantName, articleContents, error }) => {
       <CardSection>
         <View>
           <Text>
-            {sliceArticle(articleContents)}
+            {articleContents.length > 72 ? `${articleContents.slice(0,73)} ...` : articleContents}
           </Text>
         </View>
       </CardSection>
