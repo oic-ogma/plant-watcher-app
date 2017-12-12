68b941eb9875e22591035762eb09394b8b644399
youmjww <b4234@oic.jp>
2017-12-12 16:49:40 +0900

    54 - 画像がない時に表示する画像を追加
    問題
    画像がない時に表示する画像がない
    
    解決策
    IMAGE NOT FOUNDの画像を追加して最初は問答無用で表示する

--- src/components/common/ListItem.js
+++ src/components/common/ListItem.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { CardSection, Error } from '../common';
+import { Error } from '../common';
 import { Card } from 'react-native-elements';
 
 const ListItem = ({ plantName, articleContents, error }) => {
   return (
-    <Card>
+    <Card image={require('../../assets/images/imageNotFound.png')} title={plantName}>
       <Error message={error} />
-      <CardSection>
-        <View>
-          <Text>
-            {plantName}
-          </Text>
-        </View>
-      </CardSection>
-
-      <CardSection>
-        <View>
-          <Text>
-            {articleContents.length > 72 ? `${articleContents.slice(0,73)} ...` : articleContents}
-          </Text>
-        </View>
-      </CardSection>
+      <View>
+        <Text>
+          {articleContents.length > 72 ? `${articleContents.slice(0,73)} ...` : articleContents}
+        </Text>
+      </View>
     </Card>
   );
 };
